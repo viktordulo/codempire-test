3f71c476c4076e700f89af99c33e485fe5789f8f
viktordulo <victordulo96@gmail.com>
2021-01-28 14:06:31 +0200

    finished visual part

--- src/components/SlideBar.tsx
+++ src/components/SlideBar.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import style from './SlideBar.module.css'
+import '../fonts.css'
 
 const SlideBar: React.FC = () => {
     return (
         <div className = {style.wrapper}>
+            <div className={style.time}>
+                <p>17:57</p>
+            </div>
+            <div className={style.widgets}>
+                <div className={style.signal}></div>
+                <p>3G</p>
+                <div className={style.energy}></div>
+            </div>
 
         </div>
     );
